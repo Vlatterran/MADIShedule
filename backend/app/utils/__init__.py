29128c5ef00aa44922b0f5b658bad8e928f70390
Vlatterran <soboleff@mail.ru>
2022-12-11 21:38:53 +0300

    fix determining the parity of the week

--- backend/app/utils/__init__.py
+++ backend/app/utils/__init__.py
@@ -3,4 +3,4 @@ import datetime
 
 
 def is_week_even(day: time.struct_time):
-    return ((day.tm_yday + datetime.datetime(day=1, month=1, year=2022).weekday()) // 7) % 2 == 1
+    return ((day.tm_yday + datetime.datetime(day=1, month=1, year=2022).weekday()) // 7) % 2 == 0
